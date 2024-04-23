8c46b0d48959953b769955e49a8f06148e736aff
Yuichi <c0a230660d@edu.teu.ac.jp>
2024-04-23 18:05:30 +0900

    追加機能3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,6 +2,7 @@ import os
 import sys
 import random
 import pygame as pg
+import time
 
 
 WIDTH, HEIGHT = 1600, 900
@@ -29,8 +30,33 @@ def check_bound(obj_rct:pg.Rect) -> tuple[bool, bool]:
         tate = False
     return yoko, tate 
 
-
-def time():
+def GameOver(screen):
+    """
+    こうかとんと爆弾が接触したときに
+    画面を暗転させてゲームオーバーであることを知らせる
+    """
+    go_img = pg.Surface((WIDTH,HEIGHT))
+    pg.draw.rect(go_img,(0,0,0),(0,0,WIDTH,HEIGHT),width=0)
+    go_rct = go_img.get_rect()
+    go_img.set_alpha(200)
+    screen.blit(go_img,go_rct) #画面を暗くする
+    fonto = pg.font.Font(None,100)
+    txt = fonto.render("Gameover", True, (255,255,255))
+    screen.blit(txt,[600, 400])
+    koka_img = pg.transform.rotozoom(pg.image.load("fig/8.png"),0,2.0)
+    koka_rct = koka_img.get_rect()
+    koka_rct.center = 500, 450
+    screen.blit(koka_img, koka_rct) #左側のこうかとんを表示
+    koka_rct.center = 1100, 450
+    screen.blit(koka_img, koka_rct) #右側のこうかとんを表示
+    pg.display.update()
+    time.sleep(5)
+    
+    
+    
+    
+    
+def times():
     new_lst = []
     accs = [a for a in range(1,11)]
 
@@ -50,7 +76,7 @@ def main():
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
     # ここから爆弾の設定
-    accs, new_lst = time()
+    accs, new_lst = times()
     bb_img = new_lst[0]
     bd_rct = bb_img.get_rect()
     bd_rct.center = random.randint(0,WIDTH),random.randint(0,HEIGHT)
@@ -65,8 +91,9 @@ def main():
                 return
             
         if kk_rct.colliderect(bd_rct):
-            print("Game Over")
+            GameOver(screen)
             return
+        
         screen.blit(bg_img, [0, 0]) 
         #こうかとんの移動と表示
         key_lst = pg.key.get_pressed()
